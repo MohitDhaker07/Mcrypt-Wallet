ed876a7570428fc4f923f6d3a4d2ef18d4879e26
Mohit Dhaker <mohit.dhaker04@gmail.com>
2022-12-09 17:34:41 +0530

    text update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -39,7 +39,7 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-2 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white  py-1">
-            Send Crypto <br /> To Mohit
+            Send me some <br />Ether here
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world.Try sending me some...
